835eacad0c570d059106569631ba42f220122035
Rhema <reymarmcoc@gmail.com>
2025-03-09 21:21:48 +0100

    Update Navbar component to include hamburger menu and close on link click

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-// import "../index.css";
 import logo from "../netflix-logo.png";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <div className="navbar container">
         <img className="logo" src={logo} alt="logo" />
-        <nav className="navLinks">
-          <NavLink className="navlink" to="/">
+        <div className="hamburger" onClick={toggleMenu}>
+          {isOpen ? (
+            <span className="close-icon">X</span>
+          ) : (
+            <>
+              <span></span>
+              <span></span>
+              <span></span>
+            </>
+          )}
+        </div>
+        <nav className={`navLinks ${isOpen ? "active" : ""}`}>
+          <NavLink className="navlink" to="/" onClick={toggleMenu}>
             Home
           </NavLink>
-          <NavLink className="navlink" to="/tvshows">
+          <NavLink className="navlink" to="/tvshows" onClick={toggleMenu}>
             TV Shows
           </NavLink>
-          <NavLink className="navlink" to="/movies">
+          <NavLink className="navlink" to="/movies" onClick={toggleMenu}>
             Movies
           </NavLink>
-          <NavLink className="navlink" to="/new&popular">
+          <NavLink className="navlink" to="/new&popular" onClick={toggleMenu}>
             New & Popular
           </NavLink>
-          <NavLink className="navlink" to="/mylist">
+          <NavLink className="navlink" to="/mylist" onClick={toggleMenu}>
             My List
           </NavLink>
         </nav>
